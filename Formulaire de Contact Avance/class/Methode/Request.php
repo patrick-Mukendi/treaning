0a99dfff6daac95854fc300904820503f9e3cf58
Patrick mukendi <mdipatrick5@gmail.com>
2024-06-21 13:29:52 +0200

    Ce fichier permet d'envoyer et de recuperer les données des verbes html get et post

--- Formulaire de Contact Avance/class/Methode/Request.php
+++ Formulaire de Contact Avance/class/Methode/Request.php
@@ -3,7 +3,7 @@ class Request
 {
     public static function get(string $key, string $user): string
     {
-        return isset($_GET[$key])??NULL;
+        return isset($_GET[$key])??NULL; 
     }
 
     public static function post(string $key, string $user ): string
