42ae5a54bfae65e093520955cf88e8e4ab0c97bd
Patrick mukendi <mdipatrick5@gmail.com>
2024-06-24 13:48:35 +0200

    en cours

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'patrick/contactform',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '48bf13e1daf5eeb2d06809fe4f63c591646fd093',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'patrick/contactform' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '48bf13e1daf5eeb2d06809fe4f63c591646fd093',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpstan/phpstan' => array(
+            'pretty_version' => '1.11.5',
+            'version' => '1.11.5.0',
+            'reference' => '490f0ae1c92b082f154681d7849aee776a7c1443',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpstan/phpstan',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
